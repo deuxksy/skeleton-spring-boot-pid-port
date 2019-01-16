68aad9fed7dca1e5f03ed2775f2196d5b658ac27
SeokYoung.Kim <crom@zzizily.com>
2019-01-17 01:24:34 +0900

    프로그램으로 직접 구현 하는 방식

--- src/main/java/com/zzizily/tech/spring/boot/skeleton/pidport/DemoApplication.java
+++ src/main/java/com/zzizily/tech/spring/boot/skeleton/pidport/DemoApplication.java
@@ -2,12 +2,17 @@ package com.zzizily.tech.spring.boot.skeleton.pidport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.context.WebServerPortFileWriter;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication application = new SpringApplication(DemoApplication.class);
+		application.addListeners(new ApplicationPidFileWriter()); //application.pid
+		application.addListeners(new WebServerPortFileWriter()); //application.port
+		application.run(args);
 	}
 
 }
